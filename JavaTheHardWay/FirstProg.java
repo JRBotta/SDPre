ad2f9cf92aa8fbd74d94271cad540a4ceea15ed7
JRBotta <jeremy.r.botta@gmail.com>
2018-03-14 10:28:01 -0400

    ex 1-4

---
+++ JavaTheHardWay/FirstProg.java
@@ -0,0 +1,6 @@
+public class FirstProg {
+  public static void main(String[] args) {
+    System.out.println("I, Jeremy Botta, determined to learn how to code.");
+    System.out.println("Today's date is Tuesday, March 13, 2018");
+  }
+}
