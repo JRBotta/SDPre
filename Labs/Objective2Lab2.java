2f7814966d3d97c42e000d125e2524a86551d589
JRBotta <jeremy.r.botta@gmail.com>
2018-03-14 17:24:37 -0400

    completed objective 2 labs

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,13 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Charles Petereson";
+    int age = 47;
+    String state = "Arkansas";
+
+    name = "Jeremy Botta";
+    age = 27;
+    state = "Florida";
+
+    System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+  }
+}
